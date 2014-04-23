f4e0e0e53af92aae22b687aef18fb20d058c13f4
krrssna-g-s <srikrishnagumma@gmail.com>
2014-04-23 15:39:02 +0530

    Revert "latest css data updated"
    This reverts commit 780067a551242d56c206e56a127361527b8cf3c3.

--- dist/built.js
+++ dist/built.js
@@ -32,12 +32,7 @@ TSM  = (function ( $ ) {
     *             data-row-from : to tell the server which row it is from
     */
     function initModalOverlay(  ){
-       $('.modal').each(function (argument) {
-          var ht = $(this).outerHeight()
-          $(this).css({
-            bottom: -ht+'px'
-          });
-       })
+
        $('*[data-role]')
        .removeAttr('click')
        .on('click', function (argument) {
@@ -53,13 +48,12 @@ TSM  = (function ( $ ) {
             }
 
             if($role === 'modal'){
-              var $ht = $(modalTarget).outerHeight();
-              
+
               $(modalOverlay)
                .fadeIn(400, function ( ) {
                    $(modalTarget)
-                   .delay(200).show().animate({opacity: 1, bottom:0},'slow');
-                   $(modalTarget).find('.modal-content').hide();    
+                   .fadeIn('fast');
+                   $(modalTarget).find('.modal-content').hide()
                    $(modalTarget).find('.modal-content:first').show()
                 });
                 postscript(callback, fromRow);
